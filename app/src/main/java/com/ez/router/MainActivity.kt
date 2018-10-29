afb5f4ddfaa2f6e3f4b5806fcef0ddff55e6715f
shy <s20977372@gmail.com>
2018-10-30 07:17:21 +0800

    init project

---
+++ app/src/main/java/com/ez/router/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.ez.router
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
